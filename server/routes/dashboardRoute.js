7e6f7a974246deb0d2c04777daa4d5b015b1d1c3
Ani Stepanyan <anisteffanie@gmail.com>
2016-10-07 23:14:46 -0700

    [progress] adds user photos to db via gridfs

--- server/routes/dashboardRoute.js
+++ server/routes/dashboardRoute.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var fs = require('fs')
+var path = require('path')
 var router = express.Router();
 var users = require('../db/db.js');
 var session = require('client-sessions');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var Grid = require('gridfs-stream')
+var connection = mongoose.connection;
+Grid.mongo = mongoose.mongo;
 
 //middleware
 router.use(function(req, res, next){
 	if(req.session && req.session.user){
-		users.findOne({userName: req.session.user.userName}, function(err, user){
+	users.findOne({userName: req.session.user.userName}, function(err, user){
 			if(user){
 				req.user = user;
 				req.user.passWord = '';
 				req.session.user = req.user;
 				res.locals.user = req.user;
 				req.user = user;
+				
 			}
 			next();
 		});
@@ -44,8 +49,7 @@ router.post('/editProfile', function(req, res){
 	var toBeEdited = req.body.tobeEdited;
 	var set = {};
 	set[toBeEdited] = req.body.newValue;
-	console.log('set ' + req.session.user.userName)
-
+	
 	users.findOneAndUpdate({userName: req.session.user.userName}, {$set: set}, {upsert: true}, function(err){
 		console.log(err);
 	})
@@ -53,11 +57,20 @@ router.post('/editProfile', function(req, res){
 })
 
 router.post('/addPhoto', function(req, res){
-	var newPhoto= req.body.profilePic;
-	var set = {};
-	set[newPhoto] = req.body.file;
-	users.findOneAndUpdate({username: req.session.user.username}, {$set: set}, {upsert: true}, function(err){})
-	res.send(); 
-})
+	var id = req.session.user._id;
+	var photoPath = path.join(__dirname,'./temp.js')
+	connection.open('open', function(){
+		console.log('connection open');
+		var gfs = Grid(connection.db);
+		var writestream = gfs.createWriteStream({
+			filename: id
+		});
+		fs.createReadStream(photoPath).pipe(writestream);
+		writestream.on('close', function(file){
+			console.log(file.filename + " writen to db")
+		})
+	})
+		res.send(); 
+}) 
 
 module.exports = router;
